cf8dd975b426a6bc7e943c2ac47c90f710275d16
Joe Hall <jhall@alumni.fullsail.edu>
2024-02-11 13:03:21 -0500

    additional tweaks to title font.

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 import sys
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRect
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QIcon, QFont, QPixmap, QPainter
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
+
+        self.windowWidth = 1200
+        self.windowHeight = 1200
+
         super().__init__()
         self.setWindowTitle("Resu-Hunter")
         self.setWindowIcon(QIcon("images/resu_hunter_icon.png"))
-        self.setGeometry(1200,300,1200,1200)
+        self.setGeometry(1200,300,self.windowWidth,self.windowHeight)
+        self.background_image = QPixmap("images/resu_hunter_icon.png")
 
-        self.label_title = QLabel(self)
-        self.label_title.setText("Welcome to Resu-Hunter!")
-        self.label_title.setFont(QFont('Arial', 25))
+        self.label_title = QLabel("Welcome to Resu-Hunter!", self)
+        self.initialFontSize = 25
+        self.label_title.setFont(QFont('Arial', self.initialFontSize))
         self.label_title.setAlignment(Qt.AlignCenter) 
-        self.label_title.resize(self.width(), 100)
         self.updateLabelPosition()
 
         self.show()
     
     def resizeEvent(self, event):
         self.updateLabelPosition()
+        self.adjustLabelFontSize()
         super().resizeEvent(event)
 
     def updateLabelPosition(self):
         self.label_title.adjustSize()
         self.label_title.move((self.width() - self.label_title.width()) // 2, 
-                              (self.height() - self.label_title.height()) // 5)
+                              (self.height() - self.label_title.height()) // 6)
+        
+    def adjustLabelFontSize(self):
+        scaleFactor = max(self.width() / self.windowWidth, self.height() / self.windowHeight)
+        newFontSize = max(self.initialFontSize * scaleFactor, 10)
+        maxFontSize = 40
+        newFontSize = min(newFontSize, maxFontSize)
+        self.label_title.setFont(QFont('Arial', int(newFontSize)))
+        self.label_title.adjustSize()
+        self.updateLabelPosition()
+    
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(self.rect(), self.background_image)
 
 def main():
     app = QApplication(sys.argv)
